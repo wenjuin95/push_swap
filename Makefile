226ff2e518da0cabd8288c14eb1f3df5d5817e66
low <lowwenjuin27@gmail.com>
2024-01-02 15:04:10 +0800

    do push_swap

---
+++ Makefile
@@ -0,0 +1,41 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: welow < welow@student.42kl.edu.my>         +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/04/08 12:14:59 by utente            #+#    #+#              #
+#    Updated: 2023/12/31 00:33:47 by welow            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = push_swap
+ARCHIVE = push_swap.a
+CC = gcc
+CFLAGS = -Wall -Werror -Wextra -g
+MAKE_LIB = ar -rcs
+
+SRCS = $(wildcard *.c)
+OBJS = $(SRCS:.c=.o)
+
+all : $(NAME)
+
+$(NAME) : $(ARCHIVE)
+	$(CC) $< -o $@
+
+$(ARCHIVE) : $(OBJS)
+	$(MAKE_LIB) $(ARCHIVE) $^
+
+%.o : %.c 
+	$(CC) $(CFLAGS) -c $< -o $@ 
+	
+clean :
+	rm -f $(OBJS) $(ARCHIVE)
+
+fclean : clean
+	rm -f $(NAME)
+
+re : fclean all
+
+.PHONY : all clean fclea re
---
+++ sort_three.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: welow < welow@student.42kl.edu.my>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 09:31:22 by utente            #+#    #+#             */
+/*   Updated: 2024/01/02 14:51:33 by welow            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+bool	stack_sorted(t_stack *stack)
+{
+	if (!stack)
+		return (1);
+	while (stack->next)
+	{
+		if (stack->value > stack->next->value)
+			return (false);
+		stack = stack->next;
+	}
+	return (true);
+}
+
+static t_stack	*find_highest(t_stack *stack)
+{
+	int				big;
+	t_stack			*biggest_node;
+
+	if (NULL == stack)
+		return (NULL);
+	big = INT_MIN;
+	while (stack)
+	{
+		if (stack->value > big)
+		{
+			big = stack->value;
+			biggest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (biggest_node);
+}
+
+void	sort_three(t_stack **a)
+{
+	t_stack	*biggest_node;
+
+	biggest_node = find_highest(*a);
+	if (*a == biggest_node)
+		ra(a, false);
+	else if ((*a)->next == biggest_node)
+		rra(a, false);
+	if ((*a)->value > (*a)->next->value)
+		sa(a, false);
+}
+
+void	handle_five(t_stack **a, t_stack **b)
+{
+	while (stack_len(*a) > 3)
+	{
+		init_nodes(*a, *b);
+		finish_rotation(a, find_smallest(*a), 'a');
+		pb(b, a, false);
+	}
+}
