67c6fbbfbff6b4405061a9a60d433f9b10c70cf6
low <lowwenjuin27@gmail.com>
2024-01-09 17:03:44 +0800

    done

--- Makefile
+++ Makefile
@@ -6,36 +6,45 @@
 #    By: welow < welow@student.42kl.edu.my>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/08 12:14:59 by utente            #+#    #+#              #
-#    Updated: 2023/12/31 00:33:47 by welow            ###   ########.fr        #
+#    Updated: 2024/01/09 16:53:11 by welow            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = push_swap
-ARCHIVE = push_swap.a
+NAME2 = checker
 CC = gcc
-CFLAGS = -Wall -Werror -Wextra -g
-MAKE_LIB = ar -rcs
-
-SRCS = $(wildcard *.c)
+CFLAGS = -Wall -Werror -Wextra
+SRCS = 	error_free.c push.c reverse_rotate.c rotate.c \
+		sort_stack.c sort_three.c split.c stack_init.c stack_utils.c \
+		swap.c init_node.c
+MAN = push_swap.c
+BS = checker.c get_next_line.c get_next_line_utils.c
 OBJS = $(SRCS:.c=.o)
+OBJM = $(MAN:.c=.o)
+OBJB = $(BS:.c=.o)
 
 all : $(NAME)
 
-$(NAME) : $(ARCHIVE)
-	$(CC) $< -o $@
+$(NAME) : $(OBJS) $(OBJM)
+	$(CC) $(CFLAGS) $(OBJS) $(OBJM) -o $@
 
-$(ARCHIVE) : $(OBJS)
-	$(MAKE_LIB) $(ARCHIVE) $^
+$(NAME2) :$(OBJS) $(OBJB)
+	$(CC) $(CFLAGS) $(OBJS) $(OBJB) -o $@
 
 %.o : %.c 
 	$(CC) $(CFLAGS) -c $< -o $@ 
 	
 clean :
-	rm -f $(OBJS) $(ARCHIVE)
+	rm -f $(OBJS)
+	rm -f $(OBJM)
+	rm -f $(OBJB)
 
 fclean : clean
 	rm -f $(NAME)
+	rm -f $(NAME2)
 
 re : fclean all
+
+bonus: $(NAME2)
 
 .PHONY : all clean fclea re
---
+++ checker.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: welow < welow@student.42kl.edu.my>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 16:48:34 by welow             #+#    #+#             */
+/*   Updated: 2024/01/09 16:48:34 by welow            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		++s1;
+		++s2;
+	}
+	return (*s1 - *s2);
+}
+
+void	del_error(t_stack **a, t_stack **b)
+{
+	free_stack(a);
+	free_stack(b);
+	write(2, "Error\n", 6);
+	exit(1);
+}
+
+void	parse_cmd(t_stack **a, t_stack **b, char *c)
+{
+	if (!ft_strcmp(c, "pa\n"))
+		pa(a, b, true);
+	else if (!ft_strcmp(c, "pb\n"))
+		pb(b, a, true);
+	else if (!ft_strcmp(c, "rra\n"))
+		rra(a, true);
+	else if (!ft_strcmp(c, "rrb\n"))
+		rrb(b, true);
+	else if (!ft_strcmp(c, "rrr\n"))
+		rrr(a, b, true);
+	else if (!ft_strcmp(c, "ra\n"))
+		ra(a, true);
+	else if (!ft_strcmp(c, "rb\n"))
+		rb(b, true);
+	else if (!ft_strcmp(c, "rr\n"))
+		rr(a, b, true);
+	else if (!ft_strcmp(c, "sa\n"))
+		sa(a, true);
+	else if (!ft_strcmp(c, "sb\n"))
+		sb(b, true);
+	else if (!ft_strcmp(c, "ss\n"))
+		ss(a, b, true);
+	else
+		del_error(a, b);
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+	char	*next_line;
+	int		len;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1)
+		return (0);
+	else if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+	stack_init(&a, argv + 1);
+	len = stack_len(a);
+	next_line = get_next_line(0);
+	while (next_line)
+	{
+		parse_cmd(&a, &b, next_line);
+		next_line = get_next_line(0);
+	}
+	if (stack_sorted(a) && stack_len(a) == len)
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
+	free(a);
+}
