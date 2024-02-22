9a308577d7e51d98ca92bc1fdb3d94b4c587128d
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-02-22 13:09:31 +0800

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -32,20 +32,20 @@ typedef struct s_stack
 	struct s_stack		*prev;
 }				t_stack;	
 
-//*** Handle input ./push_swap "1 -42 1337" ***
+//handle input
 char			**ft_split(char const *s, char separator);
 int				ft_strlen(char const *str);
 char			*ft_strdup(const char *s);
 char			*ft_strjoin(char const *s1, char const *s2);
 
-//*** Handle errors-free ***
+//handle errors
 void			free_split(char **argv);
 void			error_free(t_stack **a);
 void			free_stack(t_stack **stack);
 int				error_duplicate(t_stack *a, int nbr);
 int				error_syntax(char *str);
 
-//*** Stack creation ***
+//stack
 char			**make_input(char **argv);
 void			stack_init(t_stack **a, char **argv, int argc);
 void			init_nodes(t_stack *a, t_stack *b);
@@ -53,7 +53,7 @@ void			set_current_position(t_stack *stack);
 void			set_push_cost(t_stack *a, t_stack *b);
 void			set_cheapest(t_stack *b);
 
-//*** linked list utils ***
+//linked list
 void			append_node(t_stack **stack, int nbr);
 t_stack			*find_last_node(t_stack *head);
 t_stack			*find_smallest(t_stack *stack);
@@ -62,12 +62,12 @@ int				stack_sorted(t_stack *stack);
 int				stack_len(t_stack *stack);
 void			finish_rotation(t_stack **s, t_stack *n, char c);
 
-//*** Algorithms ***
+//sort
 void			sort_three(t_stack **a);
 void			handle_five(t_stack **a, t_stack **b);
 void			sort_stack(t_stack **a, t_stack **b);
 
-//*** Commands ***
+//commands
 void			sa(t_stack **a, int checker);
 void			sb(t_stack **b, int checker);
 void			ss(t_stack **a, t_stack **b, int checker);
